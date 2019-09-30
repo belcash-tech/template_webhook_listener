8b2ee3ba4378dfdc6a756ee57504b6c6ebc8bf2a
Yared Getachew <veduket@gmail.com>
2019-09-30 21:21:58 +0300

    Added webhook viz

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var bodyParser = require('body-parser')
 var Logger = require('./logger')
 var jc = require('json-cycle')
 var app = express()
+var SocketIO = require('socket.io')
+var server = require('http').Server(app)
+var io = SocketIO(server)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(bodyParser.json({ type: 'application/*+json' }))
@@ -19,9 +22,11 @@ const allowCrossDomain = function(req, res, next) {
   res.setHeader('X-Application-Category', 'WebHookListener')
   next();
 }
-
 app.use(allowCrossDomain)
-app.use(express.static({ root: path.resolve('./public')}))
+app.use(express.static(path.resolve('./public/')))
+app.get('/',(req, res) => {
+  res.sendFile('index.html')
+})
 app.all('/webhook/:entity', (req, res) => {
   var status =JSON.stringify({
     params: req.params,
@@ -30,12 +35,13 @@ app.all('/webhook/:entity', (req, res) => {
     body: req.body,
     headers: req.headers
   })
+  io.sockets.emit('data',JSON.parse(status))
   console.log(`\n-------------------TIME : ${(new Date()).toISOString().slice(0,19)}-------------------------------------\n`)
   console.log(JSON.parse(status))
   Logger.debug(status)
   res.status(200).json({ message: 'Received'})
 })
-app.listen(PORT,function(){
+server.listen(PORT,function(){
   Logger.debug(`Server listening on PORT : ${PORT}`)
   console.log(`Server listening on PORT : ${PORT}`)
 })
